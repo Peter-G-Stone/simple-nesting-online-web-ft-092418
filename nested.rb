9ecf7c58b557a6e9fc6068b2409ad3c10be1b2a3
Peter Stone <peter.stone.online@gmail.com>
2018-09-27 18:35:41 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -129,5 +129,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:known_for] =
+  programmer_hash[:dennis_ritchie][:known_for].split
+  programmer_hash[:dennis_ritchie][:known_for] << "Assembly"
+  programmer_hash
 end
