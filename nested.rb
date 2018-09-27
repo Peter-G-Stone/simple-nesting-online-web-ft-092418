00a67a287548b5ad2411b1cb2c1edaf642186f9e
Peter Stone <peter.stone.online@gmail.com>
2018-09-27 18:29:28 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -59,8 +59,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  binding.pry    
-  programmer_hash[:dennis_ritchie][:languages]
+      
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
